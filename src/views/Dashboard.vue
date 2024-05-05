74015b7143539210c9e1d82cf6b6c18d0bf52824
Leon Gaban <leongaban@gmail.com>
2024-05-05 14:15:43 -0500

    first commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main className="main-container">
+    <h1>This is the dashboard</h1>
+    <div className="parent">
+      <div className="div1">
+        <p id="main-logo">COIN AVERAGE</p>
+        Left Side Bar
+      </div>
+      <div className="div2">Table Columns</div>
+      <div className="div3">
+        <section>
+          <SearchHeader />
+        </section>
+        Right Side Bar
+      </div>
+    </div>
+  </main>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy(), // for IE11 support
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        
+      },
+    },
+  },
+});
